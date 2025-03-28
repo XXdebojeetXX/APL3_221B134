d20e1cfb193619cbf241beb4ad58170e5c9524a0
221B134 <221B134@juet.co.in>
2025-03-28 09:39:54 +0530

    deleted files

--- LE5/Test.java
+++
@@ -1,8 +0,0 @@
-class Test{
-    public static void main(String []args){
-        Offering offering= new Coffee();
-        offering= new Rum(offering);
-        offering= new Rum(offering);
-        System.out.println(offering.getName() + " " + offering.getPrice());
-    }
-}
